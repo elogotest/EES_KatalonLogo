be8f0b979773eff28dd8e92c9b6ae5cf3191a4cb
Ali Kara <ali.kara@virgosol.com>
2021-11-22 17:07:35 +0300

    22.11.2021 Güncelleme

---
+++ Reports/20211119_164526/Suite Repository/e-Arsiv/20211119_164532/cucumber_report/1637330439006/html/report.js
@@ -0,0 +1,210 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/DELL/Documents/GitHub/EES_KatalonLogo/Include/features/e-Arsiv/e-Arsiv Hareketleri/e-Arsiv Raporlari/XML.feature");
+formatter.feature({
+  "name": "e-Arsiv Raporlari XML",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "name": "e-Arsiv Raporlari XML",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "Kullanıcı adı \u003ckullaniciAdi\u003e ve Şifre \u003csifre\u003e olarak giriş yapılır",
+  "keyword": "* "
+});
+formatter.step({
+  "name": "Ana menüden e-Arşiv seçilir",
+  "keyword": "* "
+});
+formatter.step({
+  "name": "Alt menü e-Arşiv Hareketleri başlığı altındaki e-Arşiv Raporları seçilir",
+  "keyword": "* "
+});
+formatter.step({
+  "name": "Filtrelerden Dönem Tarihi filtresine \u003cfiltreDegeri\u003e dropdowndan degeri seçilir",
+  "keyword": "* "
+});
+formatter.step({
+  "name": "Filtre butonlarından Listele seçilir",
+  "keyword": "* "
+});
+formatter.step({
+  "name": "Tabloda veri var mı kontrol edilir",
+  "keyword": "* "
+});
+formatter.step({
+  "name": "Tablodan \u003cveri_Adi\u003e isimli veri seçilir",
+  "keyword": "* "
+});
+formatter.step({
+  "name": "Butonlardan XML isimli butona tıklanır",
+  "keyword": "* "
+});
+formatter.step({
+  "name": "Yeni açılan sekmeye geçilir",
+  "keyword": "* "
+});
+formatter.step({
+  "name": "\u003ctag\u003e tagli ubl geldi mi",
+  "keyword": "* "
+});
+formatter.step({
+  "name": "Ana sekmeye geçilir",
+  "keyword": "* "
+});
+formatter.step({
+  "name": "Kullanıcı işlemlerinden Çıkış seçilir",
+  "keyword": "* "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "kullaniciAdi",
+        "sifre",
+        "filtreDegeri",
+        "veri_Adi",
+        "tag"
+      ]
+    },
+    {
+      "cells": [
+        "ELOGO6",
+        "123456",
+        "2020 Aralık",
+        "2691121",
+        "arsiv:eArsivRaporu"
+      ]
+    }
+  ]
+});
+formatter.scenario({
+  "name": "e-Arsiv Raporlari XML",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "Kullanıcı adı ELOGO6 ve Şifre 123456 olarak giriş yapılır",
+  "keyword": "* "
+});
+formatter.match({
+  "location": "girisSayfasi.login(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Ana menüden e-Arşiv seçilir",
+  "keyword": "* "
+});
+formatter.match({
+  "location": "menu.anaMenuSecimi(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Alt menü e-Arşiv Hareketleri başlığı altındaki e-Arşiv Raporları seçilir",
+  "keyword": "* "
+});
+formatter.match({
+  "location": "menu.basligaGoreAltMenuSecimi(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Filtrelerden Dönem Tarihi filtresine 2020 Aralık dropdowndan degeri seçilir",
+  "keyword": "* "
+});
+formatter.match({
+  "location": "filtreler.filtreDropdownSec(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Filtre butonlarından Listele seçilir",
+  "keyword": "* "
+});
+formatter.match({
+  "location": "filtreler.filtreButonSec(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Tabloda veri var mı kontrol edilir",
+  "keyword": "* "
+});
+formatter.match({
+  "location": "tablo.tablodaVeriVarMi()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Tablodan 2691121 isimli veri seçilir",
+  "keyword": "* "
+});
+formatter.match({
+  "location": "tablo.tablodanVeriSecilir(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Butonlardan XML isimli butona tıklanır",
+  "keyword": "* "
+});
+formatter.match({
+  "location": "butonlar.butonaTikla(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Yeni açılan sekmeye geçilir",
+  "keyword": "* "
+});
+formatter.match({
+  "location": "sekmeler.yeniAcilanSekmeyeGecilir()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "arsiv:eArsivRaporu tagli ubl geldi mi",
+  "keyword": "* "
+});
+formatter.match({
+  "location": "sekmeler.ublGeldiMi(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Ana sekmeye geçilir",
+  "keyword": "* "
+});
+formatter.match({
+  "location": "sekmeler.anaSekmeyeGecilir()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Kullanıcı işlemlerinden Çıkış seçilir",
+  "keyword": "* "
+});
+formatter.match({
+  "location": "kullaniciIslemleri.kullaniciIslemi(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+});
