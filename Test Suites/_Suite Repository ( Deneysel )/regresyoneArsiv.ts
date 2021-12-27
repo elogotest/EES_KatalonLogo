2a7355ff00318c078021c1d2501c58663821ec2c
Ali Kara <ali.kara@virgosol.com>
2021-12-27 16:56:08 +0300

    Deneme123

--- Test Suites/_Suite Repository ( Deneysel )/regresyoneArsiv.ts
+++ Test Suites/_Suite Repository ( Deneysel )/regresyoneArsiv.ts
@@ -68,49 +68,49 @@
    <testCaseLink>
       <guid>1e4ecdf5-2638-4aea-be1b-21cfc8a83912</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Case Repository/Regresyon/e-Arsiv/VKN-Kagit-Hal Komisyoncu</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>4072cb54-198a-4358-9d31-ddfcd41959e6</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Case Repository/Regresyon/e-Arsiv/VKN-Kagit-Hal Satis</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>ff9f0499-aa76-42de-b301-9fbafdf4e250</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Case Repository/Regresyon/e-Arsiv/VKN-Kagit-Iade</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>9264dcc3-9806-4d51-97f8-69dbb4264b97</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Case Repository/Regresyon/e-Arsiv/VKN-Kagit-Ihrac Kayitli</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>16d94c8c-106d-422f-ac56-b5f169525612</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Case Repository/Regresyon/e-Arsiv/VKN-Kagit-Istisna</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>13c8c2eb-bf18-4e70-867f-40f59d447d7e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Case Repository/Regresyon/e-Arsiv/VKN-Kagit-Ozel Matrah</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>6ef2556a-53ee-435b-a826-c237101dc120</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Case Repository/Regresyon/e-Arsiv/VKN-Kagit-Satis</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>cf31a2f5-31f2-4a3d-ac86-95b58fe3cfc3</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Case Repository/Regresyon/e-Arsiv/VKN-Kagit-Tevkifat</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
