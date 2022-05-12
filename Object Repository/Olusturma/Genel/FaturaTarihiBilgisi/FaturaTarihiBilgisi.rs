27197266106a865e0f5dd4eb575885d2510076de
ali.kara <ali.kara@virgosol.com>
2022-05-12 13:06:49 +0300

    Gecmise Dönük senaryoların düzenlenmesi

--- Object Repository/Olusturma/Genel/FaturaTarihiBilgisi/FaturaTarihiBilgisi.rs
+++ Object Repository/Olusturma/Genel/FaturaTarihiBilgisi/FaturaTarihiBilgisi.rs
@@ -7,7 +7,7 @@
    <selectorCollection>
       <entry>
          <key>BASIC</key>
-         <value>//*[@id=&quot;reactRoot&quot;]/div[2]/div/div[1]/div/div[1]/div[3]/form/div/div[4]/div/div[2]/div/div/div/div/input</value>
+         <value>//*[@id=&quot;reactRoot&quot;]/div[2]/div/div[1]/div/div[1]/div[3]/form/div/div[3]/div/div[2]/div/div/div/div/input</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
@@ -17,6 +17,6 @@
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>//*[@id=&quot;reactRoot&quot;]/div[2]/div/div[1]/div/div[1]/div[3]/form/div/div[4]/div/div[2]/div/div/div/div/input</value>
+      <value>//*[@id=&quot;reactRoot&quot;]/div[2]/div/div[1]/div/div[1]/div[3]/form/div/div[3]/div/div[2]/div/div/div/div/input</value>
    </webElementProperties>
 </WebElementEntity>
