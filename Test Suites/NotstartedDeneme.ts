e3a7937d20da0e6dfbd9fc173432f1856029b257
ali.kara <ali.kara@virgosol.com>
2022-07-06 10:49:50 +0300

    fatura olusturma caselerinin düzenlenmesi 2

--- Test Suites/NotstartedDeneme.ts
+++ Test Suites/NotstartedDeneme.ts
@@ -207,5 +207,15 @@
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/Suite Repository/Taslak</testSuiteEntity>
       </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/Suite Repository/e-Fatura2</testSuiteEntity>
+      </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
